6947e877e849995bbb1a08d7c57021b2a8e3fce4
Damith Gunawardana <damithg@gmail.com>
2025-03-31 02:19:28 +0100

    Add destination schema, components, and API-ready structure

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,53 +1,146 @@
-import type { Struct, Schema } from '@strapi/strapi';
+import type { Schema, Struct } from '@strapi/strapi';
 
-export interface SharedSlider extends Struct.ComponentSchema {
-  collectionName: 'components_shared_sliders';
+export interface ContentEssentialInfo extends Struct.ComponentSchema {
+  collectionName: 'components_content_essential_info';
   info: {
-    displayName: 'Slider';
-    icon: 'address-book';
-    description: '';
+    displayName: 'Essential Info';
   };
   attributes: {
-    files: Schema.Attribute.Media<'images', true>;
+    accessibility: Schema.Attribute.JSON;
+    bestTimeToVisit: Schema.Attribute.String;
+    entranceFees: Schema.Attribute.JSON;
+    highlights: Schema.Attribute.JSON;
+    nearestAirport: Schema.Attribute.String;
+    openingHours: Schema.Attribute.JSON;
+    transportOptions: Schema.Attribute.JSON;
+    travelTips: Schema.Attribute.JSON;
   };
 }
 
-export interface SharedSeo extends Struct.ComponentSchema {
-  collectionName: 'components_shared_seos';
+export interface ContentFaq extends Struct.ComponentSchema {
+  collectionName: 'components_content_faqs';
   info: {
-    name: 'Seo';
-    icon: 'allergies';
-    displayName: 'Seo';
-    description: '';
+    displayName: 'FAQ';
   };
   attributes: {
-    metaTitle: Schema.Attribute.String & Schema.Attribute.Required;
-    metaDescription: Schema.Attribute.Text & Schema.Attribute.Required;
-    shareImage: Schema.Attribute.Media<'images'>;
+    answer: Schema.Attribute.Text;
+    question: Schema.Attribute.String;
   };
 }
 
-export interface SharedRichText extends Struct.ComponentSchema {
-  collectionName: 'components_shared_rich_texts';
+export interface ContentFeatureItem extends Struct.ComponentSchema {
+  collectionName: 'components_content_feature_items';
   info: {
-    displayName: 'Rich text';
-    icon: 'align-justify';
-    description: '';
+    displayName: 'Feature Item';
   };
   attributes: {
-    body: Schema.Attribute.RichText;
+    description: Schema.Attribute.Text;
+    icon: Schema.Attribute.String;
+    image: Schema.Attribute.Component<'shared.gallery-image', false>;
+    title: Schema.Attribute.String;
   };
 }
 
-export interface SharedQuote extends Struct.ComponentSchema {
-  collectionName: 'components_shared_quotes';
+export interface ContentFeatureSection extends Struct.ComponentSchema {
+  collectionName: 'components_content_feature_sections';
   info: {
-    displayName: 'Quote';
-    icon: 'indent';
+    displayName: 'Feature Section';
+  };
+  attributes: {
+    items: Schema.Attribute.Component<'content.feature-item', true>;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface ContentHighlightItem extends Struct.ComponentSchema {
+  collectionName: 'components_content_highlight_items';
+  info: {
+    displayName: 'Highlight Item';
+  };
+  attributes: {
+    title: Schema.Attribute.Text;
+  };
+}
+
+export interface ContentNearbyAttraction extends Struct.ComponentSchema {
+  collectionName: 'components_content_nearby_attractions';
+  info: {
+    displayName: 'Nearby Attraction';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    distance: Schema.Attribute.String;
+    image: Schema.Attribute.Component<'shared.gallery-image', false>;
+    link: Schema.Attribute.String;
+    name: Schema.Attribute.String;
+    slug: Schema.Attribute.String;
+  };
+}
+
+export interface ContentOverviewSection extends Struct.ComponentSchema {
+  collectionName: 'components_content_overview_sections';
+  info: {
+    displayName: 'Overview Section';
+  };
+  attributes: {
+    fullDescription: Schema.Attribute.Text;
+    image: Schema.Attribute.Component<'shared.gallery-image', false>;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface ContentQuoteBlock extends Struct.ComponentSchema {
+  collectionName: 'components_content_quote_blocks';
+  info: {
+    displayName: 'Quote Block';
+  };
+  attributes: {
+    author: Schema.Attribute.String;
+    content: Schema.Attribute.Text;
+  };
+}
+
+export interface ContentRelatedTour extends Struct.ComponentSchema {
+  collectionName: 'components_content_related_tours';
+  info: {
+    displayName: 'Related Tour';
+  };
+  attributes: {
+    currency: Schema.Attribute.String;
+    duration: Schema.Attribute.String;
+    image: Schema.Attribute.Component<'shared.gallery-image', false>;
+    link: Schema.Attribute.String;
+    name: Schema.Attribute.String;
+    slug: Schema.Attribute.String;
+    startingFrom: Schema.Attribute.Decimal;
+    summary: Schema.Attribute.Text;
+  };
+}
+
+export interface ContentVideoBlock extends Struct.ComponentSchema {
+  collectionName: 'components_content_video_blocks';
+  info: {
+    displayName: 'Video Block';
   };
   attributes: {
     title: Schema.Attribute.String;
-    body: Schema.Attribute.Text;
+    videoUrl: Schema.Attribute.String;
+  };
+}
+
+export interface SharedGalleryImage extends Struct.ComponentSchema {
+  collectionName: 'components_shared_gallery_images';
+  info: {
+    displayName: 'Gallery Image';
+    icon: 'image';
+  };
+  attributes: {
+    alt: Schema.Attribute.String;
+    caption: Schema.Attribute.String;
+    orientation: Schema.Attribute.Enumeration<
+      ['landscape', 'portrait', 'square']
+    >;
+    publicId: Schema.Attribute.String;
   };
 }
 
@@ -62,14 +155,76 @@ export interface SharedMedia extends Struct.ComponentSchema {
   };
 }
 
+export interface SharedQuote extends Struct.ComponentSchema {
+  collectionName: 'components_shared_quotes';
+  info: {
+    displayName: 'Quote';
+    icon: 'indent';
+  };
+  attributes: {
+    body: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface SharedRichText extends Struct.ComponentSchema {
+  collectionName: 'components_shared_rich_texts';
+  info: {
+    description: '';
+    displayName: 'Rich text';
+    icon: 'align-justify';
+  };
+  attributes: {
+    body: Schema.Attribute.RichText;
+  };
+}
+
+export interface SharedSeo extends Struct.ComponentSchema {
+  collectionName: 'components_shared_seos';
+  info: {
+    description: '';
+    displayName: 'Seo';
+    icon: 'allergies';
+    name: 'Seo';
+  };
+  attributes: {
+    metaDescription: Schema.Attribute.Text & Schema.Attribute.Required;
+    metaTitle: Schema.Attribute.String & Schema.Attribute.Required;
+    shareImage: Schema.Attribute.Media<'images'>;
+  };
+}
+
+export interface SharedSlider extends Struct.ComponentSchema {
+  collectionName: 'components_shared_sliders';
+  info: {
+    description: '';
+    displayName: 'Slider';
+    icon: 'address-book';
+  };
+  attributes: {
+    files: Schema.Attribute.Media<'images', true>;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
-      'shared.slider': SharedSlider;
-      'shared.seo': SharedSeo;
-      'shared.rich-text': SharedRichText;
-      'shared.quote': SharedQuote;
+      'content.essential-info': ContentEssentialInfo;
+      'content.faq': ContentFaq;
+      'content.feature-item': ContentFeatureItem;
+      'content.feature-section': ContentFeatureSection;
+      'content.highlight-item': ContentHighlightItem;
+      'content.nearby-attraction': ContentNearbyAttraction;
+      'content.overview-section': ContentOverviewSection;
+      'content.quote-block': ContentQuoteBlock;
+      'content.related-tour': ContentRelatedTour;
+      'content.video-block': ContentVideoBlock;
+      'shared.gallery-image': SharedGalleryImage;
       'shared.media': SharedMedia;
+      'shared.quote': SharedQuote;
+      'shared.rich-text': SharedRichText;
+      'shared.seo': SharedSeo;
+      'shared.slider': SharedSlider;
     }
   }
 }
