5fd76b261e1e0b1af0a07022eaa1bf5fc7ea828b
Damith <damithg@gmail.com>
2025-05-18 02:41:38 +0100

    add missing routes

---
+++ src/api/experience/routes/experience.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  routes: [
+    {
+      method: 'GET',
+      path: '/experiences',
+      handler: 'experience.find',
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: 'GET',
+      path: '/experiences/:id',
+      handler: 'experience.findOne',
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: 'POST',
+      path: '/experiences',
+      handler: 'experience.create',
+      config: {
+        auth: false,
+      },
+    },
+  ],
+};
