e51ea2a48d90233bbe1918e2703e8dd680d6e1dc
damithg <damithg@gmail.com>
2025-05-22 14:34:08 +0100

    Update experience.js

--- src/api/experience/controllers/experience.js
+++ src/api/experience/controllers/experience.js
@@ -2,4 +2,42 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::experience.experience');
+module.exports = createCoreController('api::experience.experience', ({ strapi }) => ({
+  async find(ctx) {
+    const { query } = ctx;
+
+    const entities = await strapi.entityService.findMany('api::experience.experience', {
+      ...query,
+      populate: {
+        card: {
+          populate: { image: true },
+        },
+        galleryImage: true,
+        seo: {
+          populate: {
+            metaImage: {
+              fields: ['url', 'alternativeText', 'caption'],
+            },
+          },
+        },
+        relatedExperiences: {
+          populate: {
+            card: { populate: { image: true } },
+            galleryImage: true,
+            seo: {
+              populate: {
+                metaImage: {
+                  fields: ['url', 'alternativeText', 'caption'],
+                },
+              },
+            },
+            highlights: true
+          },
+        },
+      },
+    });
+
+    const sanitized = await this.sanitizeOutput(entities, ctx);
+    return this.transformResponse(sanitized);
+  }
+}));
